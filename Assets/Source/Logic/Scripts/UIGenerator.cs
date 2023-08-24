9c2457a78de9761aab23c3fead4b75b3a7dae6d2
jerffesonj <jerffesonj@gmail.com>
2023-08-24 11:54:03 -0300

    Schematic  text height

--- Assets/Source/Logic/Scripts/UIGenerator.cs
+++ Assets/Source/Logic/Scripts/UIGenerator.cs
@@ -19,14 +19,16 @@ public class UIGenerator : MonoBehaviour
     {
         List<UITextBlock> txtBlocks = new List<UITextBlock>();
 
-        var rightText = DrawText("", txtBlockRightPrefab, canvasRight, false);
+        var rightText = DrawText("", 0,txtBlockRightPrefab, canvasRight, false);
 
+        int index = 0;
         foreach (var element in allElements)
         {
             if (element.SchematicItem.element._writePartOnDoc)
             {
-                txtBlocks.Add(DrawText("> " + element, txtBlockLeftPrefab, canvasLeft, true));
+                txtBlocks.Add(DrawText(element.SchematicItem.element.ElementName, element.SchematicItem._origin, txtBlockLeftPrefab, canvasLeft, true));
                 rightText.AddLine(element.SchematicItem.ToString());
+                index += 1;
             }
         }
 
@@ -46,15 +48,15 @@ public class UIGenerator : MonoBehaviour
     {
         UITextBlock txtBlock = Instantiate(txtPrefab, parent);
         txtBlock.gameObject.name = $"TXT - {element.SchematicItem.ToString()}";
-        txtBlock.SetupText(element, setPosition);
+        txtBlock.SetupText(element,txtBlock.transform.childCount, setPosition);
         return txtBlock;
     }
 
-    public UITextBlock DrawText(string text, UITextBlock txtPrefab, RectTransform parent, bool setPosition)
+    public UITextBlock DrawText(string text, float yDistance, UITextBlock txtPrefab, RectTransform parent, bool setPosition)
     {
         UITextBlock txtBlock = Instantiate(txtPrefab, parent);
-        txtBlock.gameObject.name = $"TXT - {text}";
-        txtBlock.SetupText(text, 0, setPosition);
+        txtBlock.gameObject.name = $"TXT - >  {text}";
+        txtBlock.SetupText(text, (yDistance * (-1))/200, setPosition); ;
         return txtBlock;
     }
 
