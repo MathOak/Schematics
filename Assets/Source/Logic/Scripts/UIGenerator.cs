f0551cf6bc35342681c76e99130783c80458a9a6
Fellipe Peixoto <devfellipepeixoto@gmail.com>
2023-11-21 16:14:46 -0300

    Straight Line to part

--- Assets/Source/Logic/Scripts/UIGenerator.cs
+++ Assets/Source/Logic/Scripts/UIGenerator.cs
@@ -77,6 +77,17 @@ public partial class UIGenerator : MonoBehaviour
         
         canvasRight.gameObject.SetActive(schematic._isDiagram);
 
+        List<SchematicItem> surfaceSchematicItem = schematicItems.FindAll((item) => DrawOrder.IsFromSurface(item.element.Key));
+
+        surfaceSchematicItem.Sort((a, b) =>
+        {
+            return DrawOrder.GetGreater(a.element.Key, b.element.Key);
+        });
+
+        schematicItems.RemoveAll((item) => DrawOrder.IsFromSurface(item.element.Key));
+
+        schematicItems.InsertRange(0, surfaceSchematicItem);
+
         foreach (SchematicItem sItem in schematicItems)
         {
             UITextBlockLeft textBlock = null;
