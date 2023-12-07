519c02ae9d6dc8183e3d863fc854cb332f9ae2d2
Fellipe Peixoto <devfellipepeixoto@gmail.com>
2023-12-07 10:14:48 -0300

    WIP migration

--- Assets/Source/Logic/Scripts/ElementMigration.cs
+++ Assets/Source/Logic/Scripts/ElementMigration.cs
@@ -1,6 +1,9 @@
 using Sirenix.OdinInspector;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "part_name", menuName = "Part/Create Part")]
@@ -30,6 +33,10 @@ public class ElementImage
     public SortingLayer layer;
     public int orderInLayer = 0;
     Vector2 offset = Vector2.zero;
+    
+    public bool fixedSize;
+    [ShowIf("fixedSize")]
+    public Vector2 size;
 }
 
 [System.Serializable]
@@ -44,4 +51,22 @@ public enum ElementType
 {
     Column,
     Head
+}
+
+[Serializable]
+public struct SortingLayerPicker
+{
+    public int Layer;
+}
+
+[CustomPropertyDrawer(typeof(SortingLayerPicker))]
+public class SortingLayerTest : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var valueProp = property.FindPropertyRelative("Layer");
+
+        var layers = SortingLayer.layers.Select(x => x.name).ToArray();
+        valueProp.intValue = EditorGUI.Popup(position, label.text, valueProp.intValue, layers);
+    }
 }
