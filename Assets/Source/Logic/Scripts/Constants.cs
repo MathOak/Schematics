0c1904dbc90018b06686736c12da14df4a37c0fc
Fellipe Peixoto <devfellipepeixoto@gmail.com>
2024-01-05 09:34:13 -0300

    Constant render size

--- Assets/Source/Logic/Scripts/Constants.cs
+++ Assets/Source/Logic/Scripts/Constants.cs
@@ -3,8 +3,8 @@
     public const float DRAW_LIMITS_HORIZONTAL = 4f;
     public const float DRAW_WELL_SIZE = 2f;
 
-    public const int MAX_WIDTH = 2550;
-    public const int MAX_HEIGHT = 4000;
+    public const int MAX_WIDTH = 1748;
+    public const int MAX_HEIGHT = 2480;
 
     public const int DEFAULT_CHARS_LIMIT = 65;
 }
